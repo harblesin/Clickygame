a4abc18c62508cc2820c87ade397398b73a4b8be
ylech <waltermelonson@gmail.com>
2019-06-24 18:32:53 -0700

    Small changes to mess with an other idea for using arrays or using a change of state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,21 +5,25 @@ import Image from "./Components/Image";
 import Content from "./Components/Content";
 import pics from "./pictures.json";
 
-var newPics =[]
+var newPics = [];
 
 class App extends Component {
   state = {
     pics,
-    newPics,
-    score: 0
+    score: 0,
+    clicked: false
   };
 
   imageClicked = id => {
-    this.setState({score: this.state.score + 1 })
-    const newPics = this.state.pics.filter(pic => pic.id !== id);
+    if (this.state.clicked == false) {
+      this.setState({ score: this.state.score + 1 });
+      //const pics = this.state.pics.filter(pic => pic.id !== id);
+      this.setState({ clicked: (this.state.clicked = true) });
 
-    this.setState({ pics });
-    this.setState({newPics})
+      this.setState({ pics });
+    } else alert("You already clicked that!");
+
+    //this.setState({ newPics });
   };
 
   // alreadyClicked = id => {
@@ -28,26 +32,26 @@ class App extends Component {
   // }
 
   alreadyClicked = id => {
-    console.log("Fore sure")
-  }
+    console.log("Fore sure");
+  };
 
   render() {
     return (
       <div className="App">
-        <Navbar score = {this.state.score} />
+        <Navbar score={this.state.score} />
         <Content>
           {this.state.pics.map(pic => (
-            <Image 
-            id = {pic.id}
-            key = {pic.id}
-            imageClicked = {this.imageClicked}
-            url={pic.url} />
+            <Image
+              id={pic.id}
+              clicked={this.state.clicked}
+              key={pic.id}
+              imageClicked={this.imageClicked}
+              url={pic.url}
+            />
           ))}
-          {this.state.newPics.map(pic=> (
-            <Image 
-            key={pic.id}
-            url = {pic.url} />
-          ))}
+          {/* {this.state.newPics.map(pic => (
+            <Image key={pic.id} url={pic.url} />
+          ))} */}
         </Content>
       </div>
     );
