3bf413d55a9e5cc2845a4994480e283202c90b2b
h f <waltermelonson@gmail.com>
2019-06-29 21:28:20 -0700

    working

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("my-app/build"));
 }
 
 app.listen(PORT, function() {
